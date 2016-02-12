36b2bde3c1b38e528337cc5dda368cda7d6b1915
Tyler Crumpton <tyler.crumpton@synapse-wireless.com>
2016-02-12 12:35:25 -0600

    Make project into a library instead of script.

--- index.js
+++ index.js
@@ -4,78 +4,64 @@ var async = require("async");
 var config = require("config");
 var jsonfile = require('jsonfile')
 
-var url = 'https://api.github.com/search/code';
-var headers = { 'cache-control': 'no-cache',
-                'content-type': 'application/json',
-                'user-agent': 'github-metatag',
-                'authorization': 'token ' + config.get('api_key') };
 
-var outFile = config.get('output');
 
-var alloptions = { method: 'GET',
-  url: url,
-  qs: { q: 'meta-tags in:readme user:' + config.get('username') },
-  headers: headers };
+module.exports = {
+  getTagMap: function(username, apiKey, callback) {
+    var searchUrl = 'https://api.github.com/search/code';
+    var headers = { 'cache-control': 'no-cache',
+                    'content-type': 'application/json',
+                    'user-agent': 'github-metatag' };
+    var searchParams = { q: 'meta-tags in:readme user:' + username };
 
-var uniqueTags = FastSet();
+    // Set the auth header if a key was provided:
+    if (apiKey !== undefined) {
+      headers['authorization'] = 'token ' + apiKey;
+    }
 
-var tagMap = {};
+    var requestOptions = { method: 'GET',
+                           url: searchUrl,
+                           qs: searchParams,
+                           headers: headers };
 
-console.log("Getting metatags in repos for user: " + config.get('username'));
+    request(requestOptions, function (error, response, body) {
+      if (error) throw new Error(error);
 
-request(alloptions, function (error, response, body) {
-  if (error) throw new Error(error);
+      var tagMap = {};
 
-  repos = JSON.parse(body);
-  //console.log(repos);
-  //response['items'].forEach(function(repo) {
-  async.each(repos['items'], function(repo, done) {
-      var repoName = repo['repository']['name'];
-      //console.log("Getting tags for repo: " + repo['url']);
-      var options = { 
-        method: 'GET',
-        url: repo['url'],
-        headers: headers 
-      };
-      request(options, function (error, response, body) {
+      console.log("Retrieved list of repos with metatags.");
+
+      repos = JSON.parse(body);
+      async.each(repos['items'], function(repo, done) {
+        var repoName = repo['repository']['name'];
+        console.log("Found repo: " + repoName);
+        var repoReqOptions = requestOptions;
+        repoReqOptions['url'] = repo['url']
+        request(repoReqOptions, function (error, response, body) {
+          if (error) throw new Error(error);
+
+          repoResponse = JSON.parse(body);
+          var readmeReqOptions = requestOptions;
+          readmeReqOptions['url'] = repoResponse['download_url'];
+          request(readmeReqOptions, function (error, response, body) {
+            if (error) throw new Error(error);
+
+            var metastring = body.match(/<!-- meta-tags:.*/)[0];
+            var tags = metastring.match(/vvv-[^, ]*/g);
+
+            tags = tags.map(function (tag) {
+              return tag.replace('vvv-', '');
+            });
+
+            tagMap[repoName] = tags;
+            done();
+          });
+        });
+      }, function(error) {
         if (error) throw new Error(error);
 
-        //console.log("re: " + body);
-
-        response = JSON.parse(body);
-        var options = {
-          method: 'GET',
-          url: response['download_url'],
-          headers: headers
-        };
-        //console.log("Getting README: " + response['download_url']);
-        request(options, function (error, response, body) {
-          if (error) throw new Error(error);
-          var metastring = body.match(/<!-- meta-tags:.*/)[0];
-          var tags = metastring.match(/vvv-[^, ]*/g);
-
-          tags = tags.map(function (tag) {
-            return tag.replace('vvv-', '');
-          });
-
-          tagMap[repoName] = tags;
-
-          uniqueTags = uniqueTags.union(tags);
-          done();
-        });
-
+        callback(tagMap);
       });
-  }, function(err) {
-    console.log("Repos found: " + getRepoList(tagMap));
-    console.log("Unique tags: " + uniqueTags.toArray());
-    jsonfile.writeFile(outFile, tagMap, function (err) {
-      if (err) {
-        console.error(err);
-      }
     });
-  });
-});
-
-var getRepoList = function(tagMap) {
-  return Object.keys(tagMap);
-}
+  }
+};
